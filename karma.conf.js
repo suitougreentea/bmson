206cb8e58fbdb346d1e5635b019d8d7a5c87d4b8
suitougreentea <suitougreentea@users.noreply.github.com>
2016-02-08 17:44:40 +0900

    Install json fixtures support and some modules

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*_test.js'
+      'test/**/*_test.js',
+      'test/fixtures/**/*.bmson'
     ],
 
 
@@ -28,7 +29,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*_test.js': ['browserify']
+      'test/**/*_test.js': ['browserify'],
+      'test/fixtures/**/*.bmson': ['json_fixtures']
     },
 
 
@@ -40,6 +42,11 @@ module.exports = function(config) {
         ignore: ['**/node_modules/**', '**/test/**'],
       }), 'babelify'],
       extensions: ['.js']
+    },
+
+
+    jsonFixturesPreprocessor: {
+      stripPrefix: 'test/fixtures/',
     },
 
 
